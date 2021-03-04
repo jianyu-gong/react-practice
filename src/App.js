8a619d112855c44bc02a1696f1aeee71f5f9c27b
Jianyu Gong <jianyu0503@gmail.com>
2021-03-04 01:47:53 -0500

    加入对State的使用

--- src/App.js
+++ src/App.js
@@ -1,11 +1,39 @@
+import React, { Component } from 'react';
 import './App.css';
+import Person from './Person/Person.js';
 
-function App() {
-  return (
-    <div className="App">
-      <h1>Hi, I'm a React App</h1>
-    </div>
-  );
+class App extends Component {
+
+  state = {
+    person:[
+    {name: "Jay", age: 20},
+    {name: "Yixin", age: 30},
+    {name: "Google", age: 3}]
+  }
+
+  switchNameHandler = () => {
+    console.log("Was clicked!")
+    this.setState({
+      person:[
+        {name: "Jay Gong", age: 26},
+        {name: "Yixin", age: 300},
+        {name: "Google", age: 3}
+      ]
+      })
+  }
+
+  render() {
+    return (
+      <div className="App">
+        <h1>Hi, I'm a React App</h1>
+        <p>This is working</p>
+        <button onClick={this.switchNameHandler}>Switching Names</button>
+        <Person name={this.state.person[0].name} age={this.state.person[0].age} />
+        <Person name={this.state.person[1].name} age={this.state.person[1].age}>My Hobbie is hiking.</Person>
+        <Person name={this.state.person[2].name} age={this.state.person[2].age}/>
+      </div>
+    );
+  }
 }
 
 export default App;
