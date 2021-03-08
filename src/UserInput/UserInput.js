96d522fff0a2d72e4f9816ff62edce34f6669d14
Jianyu Gong <jianyu0503@gmail.com>
2021-03-07 22:39:58 -0500

    加入作业1

---
+++ src/UserInput/UserInput.js
@@ -0,0 +1,7 @@
+import React from 'react';
+
+const userInput = () => {
+    return <input type="text"></input>
+};
+
+export default userInput;
---
+++ src/UserOutput/UserOutput.js
@@ -0,0 +1,12 @@
+import React from 'react';
+
+const userOutput = () => {
+  return (
+    <div>
+      <p>This is paragraph A</p>
+      <p>This is paragraph B</p>
+    </div>
+  )
+};
+
+export default userOutput;
