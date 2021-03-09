d56e5043b26fcd9798e4d246098bece3d2dcce22
Jianyu Gong <jianyu0503@gmail.com>
2021-03-08 21:47:54 -0500

    加入CSS格式

--- src/UserOutput/UserOutput.js
+++ src/UserOutput/UserOutput.js
@@ -1,8 +1,9 @@
 import React from 'react';
+import './UserOutput.css';
 
 const userOutput = ( props ) => {
   return (
-    <div>
+    <div className='UserOutput'>
       <p>Username: {props.userName}</p>
       <p>This is paragraph B</p>
     </div>
